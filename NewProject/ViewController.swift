c62c063f5d5f75fb1f54206950d9a844844479f5
Artyom <94391520+artemsidorenko1996@users.noreply.github.com>
2021-11-16 13:29:23 +0300

    Initial Commit

---
+++ NewProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewProject
+//
+//  Created by Artyom on 16.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
